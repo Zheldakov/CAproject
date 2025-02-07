de07cc0b83bd36254ed4c883b33d4b6a8ef3c6bb
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-02-07 18:34:13 +0300

    Добавление удаления записей которым больше 360 дней

--- users/action.py
+++ users/action.py
@@ -1,6 +1,15 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
 from users.models import Action
 
 
 def journal_user_action(user, action):
     action = Action(user=user, action=action)
+    # Определяем дату, старше которой записи будут удалены
     action.save()
+    # Удаления записей которым больше 360 дней
+    cutoff_date = timezone.now() - timedelta(days=360)
+    deleted_count, _ = Action.objects.filter(date__lt=cutoff_date).delete()
+    print("Удалено", deleted_count)
